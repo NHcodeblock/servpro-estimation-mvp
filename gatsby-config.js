4cce121a8e1ec390af733c24fedecd14d1dc7b59
Numaan Hussain <numaanhussain8688@gmail.com>
2026-08-29 16:10:53 -0500

    add google analytics and update site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `SERVPRO Estimation Tool`,
+    description: `Customer damage estimation tool for SERVPRO`,
+    author: `Numaan Hussain`,
+    siteUrl: `https://numaan-servpro-estimator.netlify.app/`,
   },
   plugins: [
     `gatsby-plugin-image`,
@@ -32,11 +32,19 @@ module.exports = {
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          "G-492WT5RKLB",
+        ],
+        pluginConfig: {
+          head: true,
+        },
       },
     },
   ],
